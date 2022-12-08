0179a61a144cf38a4cc618423fe68c4ab8628819
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-08 17:04:42 +0300

    Update BrandingInfo.cs

--- Agent.Installer.Win/Models/BrandingInfo.cs
+++ Agent.Installer.Win/Models/BrandingInfo.cs
@@ -12,17 +12,17 @@ namespace Rimot.Agent.Installer.Win.Models
 
         public string Icon { get; set; }
 
-        public byte TitleForegroundRed { get; set; } = 29;
+        public byte TitleForegroundRed { get; set; } = 255;
 
-        public byte TitleForegroundGreen { get; set; } = 144;
+        public byte TitleForegroundGreen { get; set; } = 255;
 
-        public byte TitleForegroundBlue { get; set; } = 241;
+        public byte TitleForegroundBlue { get; set; } = 255;
 
-        public byte TitleBackgroundRed { get; set; } = 70;
+        public byte TitleBackgroundRed { get; set; } = 255;
 
-        public byte TitleBackgroundGreen { get; set; } = 70;
+        public byte TitleBackgroundGreen { get; set; } = 0;
 
-        public byte TitleBackgroundBlue { get; set; } = 70;
+        public byte TitleBackgroundBlue { get; set; } = 0;
 
         public byte ButtonForegroundRed { get; set; } = 255;
 
