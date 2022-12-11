e07ba2dada0594a18d8022b11e3d4e77a372093d
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 16:47:32 +0300

    Update HostNamePromptViewModel.cs

--- Desktop.XPlat/ViewModels/HostNamePromptViewModel.cs
+++ Desktop.XPlat/ViewModels/HostNamePromptViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using Avalonia.Controls;
 using ReactiveUI;
-using Remotely.Desktop.XPlat.Services;
+using Rimot.Desktop.XPlat.Services;
 using System.Windows.Input;
 
-namespace Remotely.Desktop.XPlat.ViewModels
+namespace Rimot.Desktop.XPlat.ViewModels
 {
     public class HostNamePromptViewModel : BrandedViewModelBase
     {
