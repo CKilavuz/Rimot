6c1c0bd391243c0fd7be98ffc3e1ce9e145a53bb
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 16:16:38 +0300

    Update FileTransferWindowViewModel.cs

--- Desktop.Win/ViewModels/FileTransferWindowViewModel.cs
+++ Desktop.Win/ViewModels/FileTransferWindowViewModel.cs
@@ -1,8 +1,8 @@
-﻿using Remotely.Desktop.Core.Interfaces;
-using Remotely.Desktop.Core.Services;
-using Remotely.Desktop.Core.ViewModels;
-using Remotely.Desktop.Win.Services;
-using Remotely.Shared.Win32;
+﻿using Rimot.Desktop.Core.Interfaces;
+using Rimot.Desktop.Core.Services;
+using Rimot.Desktop.Core.ViewModels;
+using Rimot.Desktop.Win.Services;
+using Rimot.Shared.Win32;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
 
-namespace Remotely.Desktop.Win.ViewModels
+namespace Rimot.Desktop.Win.ViewModels
 {
     public class FileTransferWindowViewModel : BrandedViewModelBase
     {
@@ -45,7 +45,7 @@ namespace Remotely.Desktop.Win.ViewModels
 
             var ofd = new OpenFileDialog()
             {
-                Title = "Upload File via Remotely",
+                Title = "Upload File via Rimot",
                 Multiselect = true,
                 CheckFileExists = true,
                 InitialDirectory = Directory.Exists(userDir) ? userDir : rootDir
