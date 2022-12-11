38d42a7375b9d87ab97811f3861cd36fcc62fb2d
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 16:05:47 +0300

    Update ClipboardServiceWin.cs

--- Desktop.Win/Services/ClipboardServiceWin.cs
+++ Desktop.Win/Services/ClipboardServiceWin.cs
@@ -1,12 +1,12 @@
-﻿using Remotely.Desktop.Core.Interfaces;
-using Remotely.Shared.Utilities;
-using Remotely.Shared.Win32;
+﻿using Rimot.Desktop.Core.Interfaces;
+using Rimot.Shared.Utilities;
+using Rimot.Shared.Win32;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Remotely.Desktop.Win.Services
+namespace Rimot.Desktop.Win.Services
 {
     public class ClipboardServiceWin : IClipboardService
     {
