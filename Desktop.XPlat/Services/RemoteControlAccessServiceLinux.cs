da7397065fd07ff0f139d9e942409e6bbde424d7
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 16:44:03 +0300

    Update RemoteControlAccessServiceLinux.cs

--- Desktop.XPlat/Services/RemoteControlAccessServiceLinux.cs
+++ Desktop.XPlat/Services/RemoteControlAccessServiceLinux.cs
@@ -1,15 +1,15 @@
 ﻿using Avalonia.Threading;
-using Remotely.Desktop.Core.Interfaces;
-using Remotely.Desktop.XPlat.ViewModels;
-using Remotely.Desktop.XPlat.Views;
-using Remotely.Shared.Utilities;
+using Rimot.Desktop.Core.Interfaces;
+using Rimot.Desktop.XPlat.ViewModels;
+using Rimot.Desktop.XPlat.Views;
+using Rimot.Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Remotely.Desktop.XPlat.Services
+namespace Rimot.Desktop.XPlat.Services
 {
     public class RemoteControlAccessServiceLinux : IRemoteControlAccessService
     {
