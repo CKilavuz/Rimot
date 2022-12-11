ecf123d597552fe778bf928661acb0656e4034e9
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 16:15:37 +0300

    Update ChatWindowViewModel.cs

--- Desktop.Win/ViewModels/ChatWindowViewModel.cs
+++ Desktop.Win/ViewModels/ChatWindowViewModel.cs
@@ -1,10 +1,10 @@
-﻿using Remotely.Shared.Models;
+﻿using Rimot.Shared.Models;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Remotely.Desktop.Win.ViewModels
+namespace Rimot.Desktop.Win.ViewModels
 {
     public class ChatWindowViewModel : BrandedViewModelBase
     {
