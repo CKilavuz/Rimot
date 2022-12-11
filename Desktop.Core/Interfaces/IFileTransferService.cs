27ace544c15bdc9d6cf1da5f902b87e15b763759
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 15:31:10 +0300

    Update IFileTransferService.cs

--- Desktop.Core/Interfaces/IFileTransferService.cs
+++ Desktop.Core/Interfaces/IFileTransferService.cs
@@ -1,10 +1,10 @@
-﻿using Remotely.Desktop.Core.Services;
-using Remotely.Desktop.Core.ViewModels;
+﻿using Rimot.Desktop.Core.Services;
+using Rimot.Desktop.Core.ViewModels;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Remotely.Desktop.Core.Interfaces
+namespace Rimot.Desktop.Core.Interfaces
 {
     public interface IFileTransferService
     {
