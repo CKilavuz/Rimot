5cc4b7822ef3140b0b7e8712da97b8887d606e1f
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 16:46:42 +0300

    Update ChatWindowViewModel.cs

--- Desktop.XPlat/ViewModels/ChatWindowViewModel.cs
+++ Desktop.XPlat/ViewModels/ChatWindowViewModel.cs
@@ -1,14 +1,14 @@
 ﻿using Avalonia.Controls;
 using ReactiveUI;
-using Remotely.Desktop.XPlat.Services;
-using Remotely.Shared.Models;
+using Rimot.Desktop.XPlat.Services;
+using Rimot.Shared.Models;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Remotely.Desktop.XPlat.ViewModels
+namespace Rimot.Desktop.XPlat.ViewModels
 {
     public class ChatWindowViewModel : BrandedViewModelBase
     {
