5b28a7b6c45baa404a9020b5dec990b7985ae23c
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 15:27:45 +0300

    Update SKBitmapExtensions.cs

--- Desktop.Core/Extensions/SKBitmapExtensions.cs
+++ Desktop.Core/Extensions/SKBitmapExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Remotely.Shared;
+﻿using Rimot.Shared;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Remotely.Desktop.Core.Extensions
+namespace Rimot.Desktop.Core.Extensions
 {
     public static class SKBitmapExtensions
     {
