97ef518fb0e99fae36940d9a931bc3a57b159016
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 14:50:20 +0300

    Update DeviceInformationServiceLinux.cs

--- Agent/Services/DeviceInformationServiceLinux.cs
+++ Agent/Services/DeviceInformationServiceLinux.cs
@@ -1,14 +1,14 @@
-﻿using Remotely.Agent.Interfaces;
-using Remotely.Shared.Models;
-using Remotely.Shared.Services;
-using Remotely.Shared.Utilities;
+﻿using Rimot.Agent.Interfaces;
+using Rimot.Shared.Models;
+using Rimot.Shared.Services;
+using Rimot.Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Remotely.Agent.Services
+namespace Rimot.Agent.Services
 {
     public class DeviceInformationServiceLinux : DeviceInformationServiceBase, IDeviceInformationService
     {
