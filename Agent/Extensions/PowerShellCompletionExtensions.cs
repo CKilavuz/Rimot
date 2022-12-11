14389e1e4a9edb8aa9656b18da6bf3f1de44e3d8
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 14:36:24 +0300

    Update PowerShellCompletionExtensions.cs

--- Agent/Extensions/PowerShellCompletionExtensions.cs
+++ Agent/Extensions/PowerShellCompletionExtensions.cs
@@ -1,5 +1,5 @@
-﻿using Remotely.Shared.Models;
-using Remotely.Shared.Utilities;
+﻿using Rimot.Shared.Models;
+using Rimot.Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Remotely.Agent.Extensions
+namespace Rimot.Agent.Extensions
 {
     public static class PowerShellCompletionExtensions
     {
