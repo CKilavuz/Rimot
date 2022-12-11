4b2eb0eea79cacdcab9b6e12aa6a270ef712515a
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-11 15:38:53 +0300

    Update ViewModelBase.cs

--- Desktop.Core/ViewModels/ViewModelBase.cs
+++ Desktop.Core/ViewModels/ViewModelBase.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Remotely.Desktop.Core.ViewModels
+namespace Rimot.Desktop.Core.ViewModels
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
