7abfb6026110f913145f97089f4f92854c6d49e4
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-15 16:46:17 +0300

    Update ScriptSchedules.razor.cs

--- Server/Components/Scripts/ScriptSchedules.razor.cs
+++ Server/Components/Scripts/ScriptSchedules.razor.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
-using Remotely.Server.Pages;
-using Remotely.Server.Services;
-using Remotely.Shared.Models;
-using Remotely.Shared.Utilities;
+using Rimot.Server.Pages;
+using Rimot.Server.Services;
+using Rimot.Shared.Models;
+using Rimot.Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Remotely.Server.Components.Scripts
+namespace Rimot.Server.Components.Scripts
 {
     [Authorize]
     public partial class ScriptSchedules : AuthComponentBase
