eeab2c4f3319d14bb576834cabab2978799896c7
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-15 17:07:49 +0300

    Update PostgreSqlDbContext.cs

--- Server/Data/PostgreSqlDbContext.cs
+++ Server/Data/PostgreSqlDbContext.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Remotely.Server.Data
+namespace Rimot.Server.Data
 {
     public class PostgreSqlDbContext : AppDb
     {
