2bf43c1b5e7814773f6645daef134937f610512c
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-15 17:08:18 +0300

    Update SqlServerDbContext.cs

--- Server/Data/SqlServerDbContext.cs
+++ Server/Data/SqlServerDbContext.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Remotely.Server.Data
+namespace Rimot.Server.Data
 {
     public class SqlServerDbContext : AppDb
     {
