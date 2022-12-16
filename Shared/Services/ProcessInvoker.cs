b057206b122f08d2e88b0200fecc92b1d8d16f24
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 12:46:45 +0300

    Update ProcessInvoker.cs

--- Shared/Services/ProcessInvoker.cs
+++ Shared/Services/ProcessInvoker.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Diagnostics;
-using Remotely.Shared.Utilities;
+using Rimot.Shared.Utilities;
 
-namespace Remotely.Shared.Services
+namespace Rimot.Shared.Services
 {
     public interface IProcessInvoker
     {
