bc87af88389d71082f92c7a9ab77192e23c92973
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 09:37:35 +0300

    Update DbLogger.cs

--- Server/Services/DbLogger.cs
+++ Server/Services/DbLogger.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
-namespace Remotely.Server.Services
+namespace Rimot.Server.Services
 {
     public class DbLogger : ILogger
     {
