e96e950cdb0edd126bd98d30d108a223dfa254a1
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 14:58:58 +0300

    Update Viewer.cs

--- Desktop.Core/Services/Viewer.cs
+++ Desktop.Core/Services/Viewer.cs
@@ -1,10 +1,10 @@
-﻿using Remotely.Desktop.Core.Interfaces;
-using Remotely.Desktop.Core.Models;
-using Remotely.Desktop.Core.ViewModels;
-using Remotely.Shared.Utilities;
-using Remotely.Shared.Models;
-using Remotely.Shared.Models.RemoteControlDtos;
-using Remotely.Shared.Win32;
+﻿using Rimot.Desktop.Core.Interfaces;
+using Rimot.Desktop.Core.Models;
+using Rimot.Desktop.Core.ViewModels;
+using Rimot.Shared.Utilities;
+using Rimot.Shared.Models;
+using Rimot.Shared.Models.RemoteControlDtos;
+using Rimot.Shared.Win32;
 using System;
 using System.Collections.Concurrent;
 using System.Drawing.Imaging;
@@ -17,7 +17,7 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Remotely.Desktop.Core.Services
+namespace Rimot.Desktop.Core.Services
 {
     public class Viewer : IDisposable
     {
