193f6db827712bd259a2cce7b7f94148f83639e5
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 10:03:11 +0300

    Update Install-Win10.ps1

--- Server/wwwroot/Content/Install-Win10.ps1
+++ Server/wwwroot/Content/Install-Win10.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 .SYNOPSIS
-   Installs the Remotely Client.
+   Installs the Rimot Client.
 .DESCRIPTION
    Do not modify this script.  It was generated specifically for your account.
 .EXAMPLE
@@ -16,7 +16,7 @@ param (
 )
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-$LogPath = "$env:TEMP\Remotely_Install.txt"
+$LogPath = "$env:TEMP\Rimot_Install.txt"
 [string]$HostName = $null
 [string]$Organization = $null
 $ConnectionInfo = $null
@@ -28,7 +28,7 @@ else {
 	$Platform = "x86"
 }
 
-$InstallPath = "$env:ProgramFiles\Remotely"
+$InstallPath = "$env:ProgramFiles\Rimot"
 
 function Write-Log($Message){
 	Write-Host $Message
@@ -62,19 +62,19 @@ function Run-StartupChecks {
 	}
 }
 
-function Stop-Remotely {
-	Start-Process -FilePath "cmd.exe" -ArgumentList "/c sc delete Remotely_Service" -Wait -WindowStyle Hidden
-	Stop-Process -Name Remotely_Agent -Force -ErrorAction SilentlyContinue
-	Stop-Process -Name Remotely_Desktop -Force -ErrorAction SilentlyContinue
+function Stop-Rimot {
+	Start-Process -FilePath "cmd.exe" -ArgumentList "/c sc delete Rimot_Service" -Wait -WindowStyle Hidden
+	Stop-Process -Name Rimot_Agent -Force -ErrorAction SilentlyContinue
+	Stop-Process -Name Rimot_Desktop -Force -ErrorAction SilentlyContinue
 }
 
-function Uninstall-Remotely {
-	Stop-Remotely
+function Uninstall-Rimot {
+	Stop-Rimot
 	Remove-Item -Path $InstallPath -Force -Recurse -ErrorAction SilentlyContinue
-	Remove-NetFirewallRule -Name "Remotely ScreenCast" -ErrorAction SilentlyContinue
+	Remove-NetFirewallRule -Name "Rimot ScreenCast" -ErrorAction SilentlyContinue
 }
 
-function Install-Remotely {
+function Install-Rimot {
 	if ((Test-Path -Path "$InstallPath") -and (Test-Path -Path "$InstallPath\ConnectionInfo.json")) {
 		$ConnectionInfo = Get-Content -Path "$InstallPath\ConnectionInfo.json" | ConvertFrom-Json
 		if ($ConnectionInfo -ne $null) {
@@ -102,25 +102,25 @@ function Install-Remotely {
 
 	if ($Path) {
 		Write-Log "Copying install files..."
-		Copy-Item -Path $Path -Destination "$env:TEMP\Remotely-Win10-$Platform.zip"
+		Copy-Item -Path $Path -Destination "$env:TEMP\Rimot-Win10-$Platform.zip"
 
 	}
 	else {
 		$ProgressPreference = 'SilentlyContinue'
 		Write-Log "Downloading client..."
-		Invoke-WebRequest -Uri "$HostName/Content/Remotely-Win10-$Platform.zip" -OutFile "$env:TEMP\Remotely-Win10-$Platform.zip" 
+		Invoke-WebRequest -Uri "$HostName/Content/Rimot-Win10-$Platform.zip" -OutFile "$env:TEMP\Rimot-Win10-$Platform.zip" 
 		$ProgressPreference = 'Continue'
 	}
 
-	if (!(Test-Path -Path "$env:TEMP\Remotely-Win10-$Platform.zip")) {
+	if (!(Test-Path -Path "$env:TEMP\Rimot-Win10-$Platform.zip")) {
 		Write-Log "Client files failed to download."
 		Do-Exit
 	}
 
-	Stop-Remotely
-	Get-ChildItem -Path "C:\Program Files\Remotely" | Where-Object {$_.Name -notlike "ConnectionInfo.json"} | Remove-Item -Recurse -Force
+	Stop-Rimot
+	Get-ChildItem -Path "C:\Program Files\Rimot" | Where-Object {$_.Name -notlike "ConnectionInfo.json"} | Remove-Item -Recurse -Force
 
-	Expand-Archive -Path "$env:TEMP\Remotely-Win10-$Platform.zip" -DestinationPath "$InstallPath"  -Force
+	Expand-Archive -Path "$env:TEMP\Rimot-Win10-$Platform.zip" -DestinationPath "$InstallPath"  -Force
 
 	New-Item -ItemType File -Path "$InstallPath\ConnectionInfo.json" -Value (ConvertTo-Json -InputObject $ConnectionInfo) -Force
 
@@ -135,11 +135,11 @@ function Install-Remotely {
 		Invoke-RestMethod -Method Post -ContentType "application/json" -Uri "$HostName/api/devices" -Body $DeviceSetupOptions -UseBasicParsing
 	}
 
-	New-Service -Name "Remotely_Service" -BinaryPathName "$InstallPath\Remotely_Agent.exe" -DisplayName "Remotely Service" -StartupType Automatic -Description "Background service that maintains a connection to the Remotely server.  The service is used for remote support and maintenance by this computer's administrators."
-	Start-Process -FilePath "cmd.exe" -ArgumentList "/c sc.exe failure `"Remotely_Service`" reset=5 actions=restart/5000" -Wait -WindowStyle Hidden
-	Start-Service -Name Remotely_Service
+	New-Service -Name "Rimot_Service" -BinaryPathName "$InstallPath\Rimot_Agent.exe" -DisplayName "Rimot Service" -StartupType Automatic -Description "Background service that maintains a connection to the Rimot server.  The service is used for remote support and maintenance by this computer's administrators."
+	Start-Process -FilePath "cmd.exe" -ArgumentList "/c sc.exe failure `"Rimot_Service`" reset=5 actions=restart/5000" -Wait -WindowStyle Hidden
+	Start-Service -Name Rimot_Service
 
-	New-NetFirewallRule -Name "Remotely Desktop Unattended" -DisplayName "Remotely Desktop Unattended" -Description "The agent that allows screen sharing and remote control for Remotely." -Direction Inbound -Enabled True -Action Allow -Program "C:\Program Files\Remotely\Desktop\Remotely_Desktop.exe" -ErrorAction SilentlyContinue
+	New-NetFirewallRule -Name "Rimot Desktop Unattended" -DisplayName "Rimot Desktop Unattended" -Description "The agent that allows screen sharing and remote control for Rimot." -Direction Inbound -Enabled True -Action Allow -Program "C:\Program Files\Rimot\Desktop\Rimot_Desktop.exe" -ErrorAction SilentlyContinue
 }
 
 try {
@@ -150,14 +150,14 @@ try {
 
 	if ($Uninstall) {
 		Write-Log "Uninstall started."
-		Uninstall-Remotely
+		Uninstall-Rimot
 		Write-Log "Uninstall completed."
 		exit
 	}
 	else {
 		Write-Log "Install started."
         Write-Log
-		Install-Remotely
+		Install-Rimot
 		Write-Log "Install completed."
 		exit
 	}
