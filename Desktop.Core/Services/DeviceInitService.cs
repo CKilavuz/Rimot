8219f866cf00cf6c289bc8fa412c748c6756d6a5
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 14:56:57 +0300

    Update DeviceInitService.cs

--- Desktop.Core/Services/DeviceInitService.cs
+++ Desktop.Core/Services/DeviceInitService.cs
@@ -1,6 +1,6 @@
-﻿using Remotely.Desktop.Core.Interfaces;
-using Remotely.Shared.Utilities;
-using Remotely.Shared.Models;
+﻿using Rimot.Desktop.Core.Interfaces;
+using Rimot.Shared.Utilities;
+using Rimot.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -14,7 +14,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Remotely.Desktop.Core.Services
+namespace Rimot.Desktop.Core.Services
 {
     public interface IDeviceInitService
     {
