18e77f957b483711fcd024d6bf22adcea8ffd72f
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 12:44:33 +0300

    Update ScriptSchedule.cs

--- Shared/Models/ScriptSchedule.cs
+++ Shared/Models/ScriptSchedule.cs
@@ -1,5 +1,5 @@
-﻿using Remotely.Shared.Enums;
-using Remotely.Shared.Utilities;
+﻿using Rimot.Shared.Enums;
+using Rimot.Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,7 +9,7 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace Remotely.Shared.Models
+namespace Rimot.Shared.Models
 {
     public class ScriptSchedule
     {
