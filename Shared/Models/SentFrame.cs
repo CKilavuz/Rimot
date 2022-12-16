3bb68994071cc5e1813afce27d83f3b25ec3938f
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 12:44:48 +0300

    Update SentFrame.cs

--- Shared/Models/SentFrame.cs
+++ Shared/Models/SentFrame.cs
@@ -1,11 +1,11 @@
-﻿using Remotely.Shared.Utilities;
+﻿using Rimot.Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Remotely.Shared.Models
+namespace Rimot.Shared.Models
 {
     public struct SentFrame
     {
