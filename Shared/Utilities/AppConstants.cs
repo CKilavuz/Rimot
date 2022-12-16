d3c5293ba4f5205e7c6a0a736f278dd875ad8c27
rimot-support <119117618+rimot-support@users.noreply.github.com>
2022-12-16 12:54:26 +0300

    Update AppConstants.cs

--- Shared/Utilities/AppConstants.cs
+++ Shared/Utilities/AppConstants.cs
@@ -4,24 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Remotely.Shared.Utilities
+namespace Rimot.Shared.Utilities
 {
     public class AppConstants
     {
-        public const string DefaultProductName = "Remotely";
-        public const string DefaultPublisherName = "Translucency Software";
+        public const string DefaultProductName = "Rimot";
+        public const string DefaultPublisherName = "Rimot Software";
         public const long MaxUploadFileSize = 100_000_000;
         public const int RelayCodeLength = 4;
         public const double ScriptRunExpirationMinutes = 30;
 
-        public const string RemotelyAscii = @"
-  _____                      _       _       
- |  __ \                    | |     | |      
- | |__) |___ _ __ ___   ___ | |_ ___| |_   _ 
- |  _  // _ \ '_ ` _ \ / _ \| __/ _ \ | | | |
- | | \ \  __/ | | | | | (_) | ||  __/ | |_| |
- |_|  \_\___|_| |_| |_|\___/ \__\___|_|\__, |
-                                        __/ |
-                                       |___/ ";
+        public const string RimotAscii = @"
+  _____                     _          
+ |  __ \                   | |           
+ | |__) |_   __ ___    ___ |_|_
+ |  _  /|_| /'_ ` _ \ / _ \| __/
+ | | \ \| |/ | | | | | (_) | | 
+ |_|  \_\_||_| |_| |_|\___/\_|
+                              ";
     }
 }
